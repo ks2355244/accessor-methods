e4c535d473b6682938425b0bedc5153efed0e443
ks2355244 <ks2355244@yahoo.com>
2016-06-13 17:00:14 +0800

    2nd edit

--- company_employee.rb
+++ company_employee.rb
@@ -1,27 +1,19 @@
 class Company
   attr_reader :company_name
-  attr_accessor : employees
+  attr_accessor :employees
 
   def initialize(name)
     @company_name  = name
     @employees   = []
   end
- 
-  def company_name
-    @company_name
-  end
- 
+  
   def company_name=(new_name)
     @company_name = new_name
   end
  
   def add_employee(employee)
     @employees << employee
-  end
- 
-  def employees
-    @employees
-  end
+  end 
 end
  
  
@@ -35,25 +27,15 @@ class Employee
     @id       = rand(100_000_000).to_s.insert(2, '-').insert(6, '-')
   end
  
-  def name
-    @name
-  end
- 
   def name=(new_name)
     @name = new_name
   end
- 
-  def position
-    @position
-  end
+
  
   def position=(new_position)
     @position = new_position
   end
  
-  def id
-    @id
-  end
 end
  
 next_academy = Company.new("NEXT Academy")
